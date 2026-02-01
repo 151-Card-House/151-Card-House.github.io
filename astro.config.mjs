7c3904eeb2b16ba0d96a26d3636edf054f2f6d0c
Matthew Reilly <mtreilly@Matthews-MacBook-Air.local>
2026-02-01 15:00:30 -0500

    Add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://151-Card-House.github.io',
+  base: '/',
+});
